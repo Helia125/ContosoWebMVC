ff02fb3747ad8f7651b5b5a6dccceb9c796301cc
Hang ZHAO <hangzhao.125@gmail.com>
2018-01-16 22:32:34 -0500

    Create CourseRepository.

---
+++ Contoso.Data/CourseRepository.cs
@@ -0,0 +1,27 @@
+﻿using Contoso.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contoso.Data
+{
+    public class CourseRepository : Repository<Course>, ICourseRepository
+    {
+        public CourseRepository(ContosoDbContext context) : base(context)
+        {
+        }
+
+        public Course GetStudentByTitle(string title)
+        {
+            var course = _context.course.FirstOrDefault(s => s.Title == title);
+            return course;
+        }
+    }
+
+    public interface ICourseRepository : IRepository<Course>//create specifc method for the course
+    {
+        Course GetStudentByTitle(string title);
+    }
+}
